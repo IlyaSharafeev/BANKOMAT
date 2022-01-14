56c1795991878961d21c9a1fc84b365e13751c84
Ilya <ilyasharafeev01@gmail.com>
2022-01-14 21:34:04 +0200

    task 2 bankomat

---
+++ 1.js
@@ -0,0 +1,24 @@
+//Первый вариант
+//Вариант с бесконечным количеством купюр 
+
+function getSum(exchange) {
+   const pricePolicy = [100, 20, 10, 5, 2, 1];
+   const res = [];
+
+   if (exchange > 0) {
+      for (let i = 0; i < pricePolicy.length; i++) {
+         let bill = pricePolicy[i];
+
+         while (exchange - bill >= 0) {
+            exchange -= bill;
+            res.push(bill);
+         }
+      }
+   } else {
+      console.log("Пожалуйста введите сумму :)");
+   }
+
+   return res;
+}
+
+console.log(getSum(73));
